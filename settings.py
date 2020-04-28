ad6f7f3cd65a0114d4c060bd022c6206f5f79464
James Carr <jamescarr0@hotmail.com>
2020-04-28 16:40:13 +0100

    Basic game play completed. - Player lives and game over added.

--- settings.py
+++ settings.py
@@ -5,13 +5,14 @@ class Settings:
         """ Initialise game settings. """
 
         # Screen Settings
-        self.screen_width = 0
-        self.screen_height = 0
+        self.screen_width = 1200
+        self.screen_height = 800
         self.BACKGROUND_COLOR = (230, 230, 230)
         self.WINDOW_TITLE = "Retro Space Invaders."
 
         # Spaceship settings
-        self.SPACESHIP_SPEED = 5
+        self.SPACESHIP_SPEED = 10
+        self.SPACESHIP_LIVES = 3
 
         # Missile settings
         self.missile_speed = 8
@@ -21,7 +22,7 @@ class Settings:
         self.missile_count = 5
 
         # Alien ship settings
-        self.alien_speed = 3.0
+        self.alien_speed = 30.0
         self.alien_drop_speed = 10
         # Alien direction 1 represents RIGHT; -1 represents LEFT
         self.alien_direction = 1
--- spaceship.py
+++ spaceship.py
@@ -10,27 +10,35 @@ class Spaceship:
         self.screen_rect = si_game.screen.get_rect()
 
         # Load the spaceship image and get its rectangle.
-        self.image = pygame.transform.smoothscale((pygame.image.load('images/boris.png')), (120, 180))
-        self.image_rect = self.image.get_rect()
+        self.image = pygame.transform.smoothscale((pygame.image.load('images/spaceship.png')), (80, 120))
+        self.rect = self.image.get_rect()
 
         # Start with a new ship at the bottom center of the screen.
-        self.image_rect.midbottom = self.screen_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
         # Continuous movement flags.
         self.moving_right = False
         self.moving_left = False
 
-        self.x = float(self.image_rect.x)
+        self.x = float(self.rect.x)
 
     def move(self):
         """ Update the spaceship's position. """
-        if self.moving_right and self.image_rect.right < self.screen_rect.right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.SPACESHIP_SPEED
-        if self.moving_left and self.image_rect.left > 0:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.SPACESHIP_SPEED
 
-        self.image_rect.x = self.x
+        self.rect.x = self.x
+
+    def respawn(self):
+        """ Respawn spaceship to center position. """
+        # Center spaceship midbottom to screen midbottom position
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Reset self.x attribute to allow us to track the ships exact position.
+        self.x = float(self.rect.x)
 
     def blitme(self):
         """ blit spaceship to screen. """
-        self.screen.blit(self.image, self.image_rect)
+        self.screen.blit(self.image, self.rect)
