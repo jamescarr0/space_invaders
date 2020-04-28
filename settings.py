fbcf366e619854750584c98435fca633774c3e8b
James Carr <jamescarr0@hotmail.com>
2020-04-28 17:02:02 +0100

    Game speed updates.

--- settings.py
+++ settings.py
@@ -15,14 +15,14 @@ class Settings:
         self.SPACESHIP_LIVES = 3
 
         # Missile settings
-        self.missile_speed = 8
+        self.missile_speed = 6
         self.missile_width = 3
         self.missile_height = 15
         self.missile_color = (60, 60, 60)
         self.missile_count = 5
 
         # Alien ship settings
-        self.alien_speed = 30.0
+        self.alien_speed = 3.0
         self.alien_drop_speed = 10
         # Alien direction 1 represents RIGHT; -1 represents LEFT
         self.alien_direction = 1
